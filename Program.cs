43be6646468119775b0ecc8b4d2fd58e738a9d70
Muresan Andris <muresan.andris@yahoo.com>
2021-05-12 13:11:43 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Counting_Valleys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ActualH= 0;
+            int Valleys = 0;
+            bool SameValley = false;
+            int steps = 0;
+            String path;
+                Console.WriteLine("Input number of steps");
+                steps = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Input step sequnece");
+                path = Console.ReadLine();
+
+            foreach (char c in path)
+            {
+                if (c == 'U')
+                    ActualH++;
+                else
+                    ActualH--;
+                if (ActualH < 0 & SameValley == false)
+                {
+                    Valleys++;
+                    SameValley = true;
+                }
+                else if (ActualH >= 0 & SameValley == true)
+                {
+                    SameValley = false;
+                }
+            }
+            Console.WriteLine(Valleys);
+        }
+    }
+
+}
